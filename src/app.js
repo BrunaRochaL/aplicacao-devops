fb457c221611ebd7bd8304748f022e2b908dc7ab
ErikaChermack <erika.paola@faculdadecesusc.edu.br>
2024-04-13 17:29:00 -0300

    Criação endpoint-integrantes

--- src/app.js
+++ src/app.js
@@ -2,9 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+let data = {
+    'integrantes':[
+        {'nome':'Bruna Nizolli Guex Rocha Lourenço'},
+        {'nome':'Erika Paola Chermack'},
+        {'nome':'Heloiza Costa da Silveira'} 
+    ]
+}
+
 app.get("/", (req, res) => {
     res.sendFile("views/index.html", { root: __dirname });
   });
+
+app.get('/integrantes', (req, res) => {
+    res.json(data)
+    })
 
 app.listen(port, () => {
   console.log("Example app listening on port: " + port);
