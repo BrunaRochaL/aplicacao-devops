f851b2e2087b75f8c8b816dcd0ca33b218b2f7ae
ErikaChermack <erika.paola@faculdadecesusc.edu.br>
2024-04-13 16:49:50 -0300

    Criação aplicação app.js

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
-});
+    res.sendFile("views/index.html", { root: __dirname });
+  });
 
 app.listen(port, () => {
   console.log("Example app listening on port: " + port);
